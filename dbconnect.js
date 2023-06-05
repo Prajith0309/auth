b4cf76e37df6ef52be9cb8cf39af8a2f15ca602d
Prajith <pragithprasad12@gmail.com>
2023-06-06 00:20:09 +0530

    second commit

--- dbconnect.js
+++ dbconnect.js
@@ -1,8 +1,8 @@
-
+require('dotenv').config()
 
 const mongoose = require('mongoose')
 
-const connection = mongoose.connect("mongodb://localhost:27017/auth")
+const connection = mongoose.connect(process.env.MONGODB_URL)
 .then(()=>{
     console.log('connection successful')
 }).catch(err => console.log('connection failed', err))
